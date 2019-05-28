cfad16d082326b6ed20873a6574752c6cd18babf
iPagar <iPagar@users.noreply.github.com>
2019-05-29 00:27:51 +0300

    Add files via upload

--- pirson.js
+++ pirson.js
@@ -1,14 +1,407 @@
-let decimals;
-let q;
-let values;
-let decimalsAdd = 2;
-let dimension;
-let repeat;
-let crit = 5;
-let firstTable = [];
-let tempAr;
-let ra;
-let lastArray = [];
+let decimals = null;
+let dimension = null;
+let repeat = null;
+let values = null;
+let q = null;
+let grabbs = null;
+let system = null;
+let borderSystem = null;
+let p = 0.95;
+const decimalsAdd = 2;
+
+function showResult() {
+	let container = document.createElement("div");
+	container.id = "result";
+
+	const kvantilStudenta = prompt(
+		"Введите квантиль стьюдента при " +
+			"n = " +
+			values.length +
+			", p = 0.95"
+	);
+	const epsilon = (kvantilStudenta * sdX()).toFixed(decimals);
+	container.innerHTML += ++repeat + ") Результат измерений<br>";
+
+	if (checkCorrection()) {
+		const k = ((epsilon * borderSystem) / (sdX() * sdSystem())).toFixed(
+			decimals
+		);
+		const skoSum = Math.sqrt(
+			sdX() * sdX() + sdSystem() * sdSystem()
+		).toFixed(decimals);
+		const delta = (k * skoSum).toFixed(decimals);
+
+		container.innerHTML +=
+			katex.renderToString("x = \\bar{x} \\pm \\Delta") +
+			", p = " +
+			p +
+			"<br>";
+		container.innerHTML +=
+			"n = " +
+			values.length +
+			", p = 0.95 =>" +
+			katex.renderToString("t_{\\frac{1+p}{2}}(n-1)=") +
+			kvantilStudenta +
+			"<br>";
+		container.innerHTML +=
+			katex.renderToString(
+				"S_{\\bar{x}} = \\frac{S_{x}}{\\sqrt{n}} = \\frac{" +
+					sd() +
+					"}{\\sqrt{" +
+					values.length +
+					"}}=" +
+					sdX()
+			) +
+			"(" +
+			dimension +
+			")" +
+			"<br>";
+		container.innerHTML +=
+			katex.renderToString(
+				"\\pm\\varepsilon=\\pm t_{\\frac{1+p}{2}}(n-1)\\times S_{\\bar{x}}=\\pm(" +
+					kvantilStudenta +
+					"\\times" +
+					sdX() +
+					")=\\pm(" +
+					epsilon +
+					")"
+			) +
+			"(" +
+			dimension +
+			")" +
+			"<br>";
+		container.innerHTML +=
+			katex.renderToString(
+				"k = \\frac{\\varepsilon + \\Theta_{sum}}{S_{\\bar{x}} + S_{\\Theta}}= \\frac{" +
+					epsilon +
+					"+ " +
+					borderSystem +
+					"}{" +
+					sdX() +
+					"+" +
+					sdSystem() +
+					"}=" +
+					k
+			) + "<br>";
+		container.innerHTML +=
+			katex.renderToString(
+				"S_{\\sum} = \\sqrt{S_{\\bar{x}}^2 + S_{\\Theta}^2} = \\sqrt{" +
+					Math.pow(sdX(), 2).toFixed(decimals) +
+					"^2 + " +
+					Math.pow(sdSystem(), 2).toFixed(decimals) +
+					"^2}=" +
+					skoSum
+			) + "<br>";
+		container.innerHTML +=
+			katex.renderToString("\\Delta = k * S_{\\sum}=" + delta) + "<br>";
+
+		container.innerHTML +=
+			"x = (" +
+			avg() +
+			katex.renderToString("\\pm") +
+			delta +
+			")" +
+			dimension +
+			", p = " +
+			p +
+			"<br>";
+	} else {
+		container.innerHTML +=
+			katex.renderToString(
+				"x = \\bar{x} \\pm t_{\\frac{1+p}{2}}(n-1)\\times S_x"
+			) +
+			", p = " +
+			p +
+			"<br>";
+		container.innerHTML +=
+			"n = " +
+			values.length +
+			", p = 0.95 =>" +
+			katex.renderToString("t_{\\frac{1+p}{2}}(n-1)=") +
+			kvantilStudenta +
+			"<br>";
+		container.innerHTML +=
+			katex.renderToString(
+				"S_{\\bar{x}} = \\frac{S_{x}}{\\sqrt{n}} = \\frac{" +
+					sd() +
+					"}{\\sqrt{" +
+					values.length +
+					"}}=" +
+					sdX()
+			) +
+			"(" +
+			dimension +
+			")" +
+			"<br>";
+		container.innerHTML +=
+			"x = (" +
+			avg() +
+			katex.renderToString("\\pm") +
+			epsilon +
+			")" +
+			dimension +
+			", p = " +
+			p +
+			"<br>";
+	}
+
+	$("#test").append(container);
+}
+
+function showCorrection() {
+	let container = document.createElement("div");
+	container.id = "correction";
+	const skoSystem =
+		katex.renderToString(
+			"S_{\\Theta} = \\frac{\\Theta}{\\sqrt{3}} = \\frac{" +
+				borderSystem +
+				"}{\\sqrt{3}}=" +
+				sdSystem()
+		) +
+		"(" +
+		dimension +
+		")";
+	container.innerHTML =
+		++repeat +
+		") Исключение систематической погрешности путем введения поправок<br>" +
+		skoSystem +
+		"<br>" +
+		++repeat +
+		") Проверка условия введения поправки<br>";
+	container.innerHTML += katex.renderToString(
+		"|c| > 0.5\\frac{S_{\\Theta}^2}{S_x} = |" +
+			system +
+			"|" +
+			" > 0.5\\frac{" +
+			sdSystem() +
+			"^2}{" +
+			sd() +
+			"} =>"
+	);
+	if (checkCorrection()) {
+		container.innerHTML +=
+			katex.renderToString(
+				Math.abs(system) +
+					">" +
+					Number(0.5 * (Math.pow(sdSystem(), 2) / sd())).toFixed(
+						decimals
+					)
+			) + " => условия выполнены<br>";
+		container.innerHTML +=
+			++repeat +
+			") Введение поправки<br>" +
+			katex.renderToString("\\bar{x}' - ") +
+			"неисправленный результат<br>" +
+			katex.renderToString(
+				"\\bar{x} = \\bar{x}' + c = " +
+					avg() +
+					(+system).toFixed(decimals) +
+					"=" +
+					(+avg() + +system).toFixed(decimals)
+			) +
+			"(" +
+			dimension +
+			")";
+	} else {
+		container.innerHTML +=
+			katex.renderToString(
+				Math.abs(system) +
+					"\\le" +
+					Number(0.5 * (Math.pow(sdSystem(), 2) / sd())).toFixed(
+						decimals
+					)
+			) + " => условия не выполнены";
+	}
+	$("#test").append(container);
+}
+
+function sdX() {
+	const sdX = sd() / Math.sqrt(values.length);
+
+	return sdX.toFixed(decimals);
+}
+
+function sdSystem() {
+	const sko = borderSystem / Math.sqrt(3);
+	return sko.toFixed(decimals);
+}
+
+function checkCorrection() {
+	const check = Math.abs(system) > 0.5 * (Math.pow(sdSystem(), 2) / sd());
+	return check;
+}
+
+function avg() {
+	let total = 0;
+	for (let i = 0; i < values.length; i++) {
+		total += values[i];
+	}
+	let avg = total / values.length;
+	return avg.toFixed(decimals);
+}
+
+function sd() {
+	let total = 0;
+	for (let i = 0; i < values.length; i++) {
+		total += Math.pow(values[i] - avg(), 2);
+	}
+	const sd = Math.sqrt(total / (values.length - 1));
+	return sd.toFixed(decimals);
+}
+
+function gT() {
+	const n = values.length;
+
+	if ((q === 0.05) & (n > 75)) {
+		grabbs = 3.29;
+	} else {
+		grabbs = prompt(`Введите критерий граббса при n = ${n}, q = ${q}`);
+	}
+}
+
+function gMax() {
+	const gMax = Math.abs(Math.max(...values) - avg()) / sd();
+	return gMax.toFixed(decimals);
+}
+
+function gMin() {
+	const gMin = Math.abs(Math.min(...values) - avg()) / sd();
+	return gMin.toFixed(decimals);
+}
+
+function g() {
+	gT();
+	const min = gMin();
+	const max = gMax();
+	let error = false;
+	let container = document.createElement("div");
+	container.id = repeat;
+	$("#test").append(container);
+	$("#" + repeat).append(createTableValues());
+	$("#" + repeat).append(createFirst());
+	$("#" + repeat).append(createSecond());
+	if (min > grabbs) {
+		error = true;
+		values.shift();
+	}
+	if (max > grabbs) {
+		error = true;
+		values.pop();
+	}
+	if (error) {
+		repeat++;
+		g();
+	}
+}
+
+function createFirst() {
+	let first = document.createElement("div");
+	let formulaFirst =
+		katex.renderToString(
+			"\\bar{x} = \\frac{1}{n}\\displaystyle\\sum_{i=1}^nx_{i} = \\frac{1}{" +
+				values.length +
+				"}(" +
+				values[0] +
+				" + ... +" +
+				values[values.length - 1] +
+				") = " +
+				avg()
+		) +
+		"(" +
+		dimension +
+		")";
+	let formulaSecond =
+		katex.renderToString(
+			"S_x = \\sqrt{\\frac{1}{n - 1}\\displaystyle\\sum_{i=1}^n(x_{i} - \\bar{x})^{2}} = \\sqrt{\\frac{1}{" +
+				(values.length - 1) +
+				"}(" +
+				values[0] +
+				"-" +
+				avg() +
+				")^{2}" +
+				" + ... + (" +
+				values[values.length - 1] +
+				"-" +
+				avg() +
+				")^{2}} = " +
+				sd()
+		) +
+		"(" +
+		dimension +
+		")";
+	first.innerHTML =
+		'<div style="display: block">' +
+		repeat +
+		") Вычисляем x̅ и S<sub>x</sub> результатов измерений</div> <div>" +
+		formulaFirst +
+		"</div>" +
+		"<div>" +
+		formulaSecond +
+		"</div>";
+	return first;
+}
+
+function createSecond() {
+	let first = document.createElement("div");
+	const min = Math.min(...values);
+	const max = Math.max(...values);
+	let formulaFirst =
+		katex.renderToString("x_{max} = " + max + "(" + dimension + ")") +
+		katex.renderToString(
+			" => G_{max} = \\frac{|x_{max} - \\bar{x}|}{S_x} = \\frac{|" +
+				max +
+				"-" +
+				avg() +
+				"|}{" +
+				sd() +
+				"} = "
+		) +
+		gMax();
+	if (gMax() > grabbs)
+		formulaFirst +=
+			katex.renderToString("~=>~G_{max} > G_t~=>~x_{max}") +
+			"содержит грубую погрешность";
+	else
+		formulaFirst +=
+			katex.renderToString(" => G_{max} \\le G_t~=>~x_{max}") +
+			"не содержит грубую погрешность";
+	let formulaSecond =
+		katex.renderToString("x_{min} = " + min + "(" + dimension + ")") +
+		katex.renderToString(
+			"~=>~G_{min} = \\frac{|x_{min} - \\bar{x}|}{S_x} = \\frac{|" +
+				min +
+				"-" +
+				avg() +
+				"|}{" +
+				sd() +
+				"} = "
+		) +
+		gMin();
+	if (gMin() > grabbs)
+		formulaSecond +=
+			katex.renderToString(" => G_{min} > G_t~=>~x_{min}") +
+			"содержит грубую погрешность";
+	else
+		formulaSecond +=
+			katex.renderToString(" => G_{min} \\le G_t~=>~x_{min}") +
+			"не содержит грубую погрешность";
+	first.innerHTML =
+		'<div style="display: block">' +
+		++repeat +
+		") Выявляем результаты, содержащие грубые погрешности</div><div>n = " +
+		values.length +
+		", q = " +
+		q +
+		", => G<sub>t</sub> = " +
+		grabbs +
+		"<div>" +
+		formulaFirst +
+		"</div>" +
+		"<div>" +
+		formulaSecond +
+		"</div>";
+	return first;
+}
 
 function createTableValues() {
 	let table = document.createElement("table");
@@ -30,382 +423,10 @@ function createTableValues() {
 	return table;
 }
 
-function granicaTable(r, min, max, h) {
-	let table = document.createElement("table");
-	table.className = "table table-sm";
-	let body = document.createElement("tbody");
-
-	td = document.createElement("tr");
-	body.appendChild(td);
-	let tr = document.createElement("td");
-	tr.innerHTML = "j";
-	td.appendChild(tr);
-	tr = document.createElement("td");
-	tr.innerHTML = "x<sub>Hj</sub>, мм";
-	td.appendChild(tr);
-	tr = document.createElement("td");
-	tr.innerHTML = "x<sub>Bj</sub>, мм";
-	td.appendChild(tr);
-	tr = document.createElement("td");
-	tr.innerHTML = "m<sub>j</sub>, мм";
-	td.appendChild(tr);
-
-	const arr = [];
-
-	for (let i = 1; i <= r; i++) {
-		tr = document.createElement("tr");
-		body.appendChild(tr);
-		let temp = [];
-
-		for (let j = 0; j < 4; j++) {
-			td = document.createElement("td");
-
-			switch (j) {
-				case 0:
-					td.innerHTML = i;
-					break;
-				case 1:
-					td.innerHTML = (min + (i - 1) * h).toFixed(decimals);
-					temp.push(td.innerHTML);
-					break;
-				case 2:
-					if (i === r) td.innerHTML = max.toFixed(decimals);
-					else td.innerHTML = (min + i * h).toFixed(decimals);
-					temp.push(td.innerHTML);
-					break;
-				case 3:
-					if (i !== r)
-						td.innerHTML = values.filter(
-							value =>
-								value >= min + (i - 1) * h &&
-								value < min + i * h
-						).length;
-					else
-						td.innerHTML = values.filter(
-							value =>
-								value >= min + (i - 1) * h &&
-								value <= min + i * h
-						).length;
-					temp.push(td.innerHTML);
-					break;
-			}
-
-			tr.appendChild(td);
-		}
-		arr.push(temp);
-	}
-
-	unionIntervals(arr);
-
-	table.appendChild(body);
-	tempAr = arr;
-
-	return table;
-}
-
-function unionTable(arr) {
-	let table = document.createElement("table");
-	table.className = "table table-sm";
-	let body = document.createElement("tbody");
-
-	td = document.createElement("tr");
-	body.appendChild(td);
-	let tr = document.createElement("td");
-	tr.innerHTML = "j";
-	td.appendChild(tr);
-	tr = document.createElement("td");
-	tr.innerHTML = "x<sub>Hj</sub>, мм";
-	td.appendChild(tr);
-	tr = document.createElement("td");
-	tr.innerHTML = "x<sub>Bj</sub>, мм";
-	td.appendChild(tr);
-	tr = document.createElement("td");
-	tr.innerHTML = "m<sub>j</sub>, мм";
-	td.appendChild(tr);
-
-	for (let i = 0; i < arr.length; i++) {
-		tr = document.createElement("tr");
-		body.appendChild(tr);
-
-		for (let j = 0; j < 4; j++) {
-			td = document.createElement("td");
-
-			switch (j) {
-				case 0:
-					td.innerHTML = i + 1;
-					break;
-				default:
-					td.innerHTML = arr[i][j - 1];
-					break;
-			}
-
-			tr.appendChild(td);
-		}
-	}
-	table.appendChild(body);
-
-	let text = document.createElement("div");
-	text.innerHTML = `Объединяем интервалы`;
-	$("#test").append(text);
-	$("#test").append(table);
-
-	thirdTable(arr);
-}
-
-function lastTable() {
-	let table = document.createElement("table");
-	table.className = "table table-sm";
-	let body = document.createElement("tbody");
-
-	td = document.createElement("tr");
-	body.appendChild(td);
-	let tr = document.createElement("td");
-	tr.innerHTML = "j";
-	td.appendChild(tr);
-	tr = document.createElement("td");
-	tr.innerHTML = "z<sub>j</sub>";
-	td.appendChild(tr);
-	tr = document.createElement("td");
-	tr.innerHTML = "f<sub>z</sub>(z<sub>j</sub>)";
-	td.appendChild(tr);
-	tr = document.createElement("td");
-	tr.innerHTML = "в<sub>j</sub>";
-	td.appendChild(tr);
-
-	arr = calcLastTable(calcThirdTable(tempAr));
-
-	for (let i = 0; i < arr.length; i++) {
-		tr = document.createElement("tr");
-		body.appendChild(tr);
-
-		for (let j = 0; j < 4; j++) {
-			td = document.createElement("td");
-
-			switch (j) {
-				case 0:
-					td.innerHTML = i + 1;
-					break;
-				default:
-					td.innerHTML = arr[i][j - 1];
-					break;
-			}
-
-			tr.appendChild(td);
-		}
-	}
-	table.appendChild(body);
-	$("#test").append(table);
-}
-
-function calcLastTable(arr) {
-	const array = [];
-	const n = values.length;
-
-	for (let i = 0; i < arr.length; i++) {
-		let temp = [];
-
-		temp.push(((arr[i][1] - avg(values)) / sd(values)).toFixed(decimals));
-		temp.push(
-			(
-				(1 / Math.sqrt(2 * Math.PI)) *
-				Math.exp(-(Math.pow(temp[0], 2) / 2))
-			).toFixed(decimals)
-		);
-		temp.push(
-			((Number(arr[i][0]) * temp[1]) / sd(values)).toFixed(decimals)
-		);
-
-		array.push(temp);
-		lastArray.push(temp);
-	}
-
-	return array;
-}
-
-function avg(x) {
-	let total = 0;
-	for (let i = 0; i < x.length; i++) {
-		total += x[i];
-	}
-	let avg = total / x.length;
-	return avg.toFixed(decimals);
-}
-
-function sd(x) {
-	let total = 0;
-	for (let i = 0; i < x.length; i++) {
-		total += Math.pow(x[i] - avg(x), 2);
-	}
-	const sd = Math.sqrt(total / (x.length - 1));
-	return sd.toFixed(decimals);
-}
-
-function thirdTable(array) {
-	let table = document.createElement("table");
-	table.className = "table table-sm";
-	let body = document.createElement("tbody");
-
-	td = document.createElement("tr");
-	body.appendChild(td);
-	let tr = document.createElement("td");
-	tr.innerHTML = "j";
-	td.appendChild(tr);
-	tr = document.createElement("td");
-	tr.innerHTML = "h<sub>j</sub>, мм";
-	td.appendChild(tr);
-	tr = document.createElement("td");
-	tr.innerHTML = "x<sub>0j</sub>, мм";
-	td.appendChild(tr);
-	tr = document.createElement("td");
-	tr.innerHTML = "p^<sub>j</sub>";
-	td.appendChild(tr);
-	tr = document.createElement("td");
-	tr.innerHTML = "p*<sub>j</sub>, 1/мм";
-	td.appendChild(tr);
-
-	arr = calcThirdTable(array);
-
-	for (let i = 0; i < arr.length; i++) {
-		tr = document.createElement("tr");
-		body.appendChild(tr);
-
-		for (let j = 0; j < 5; j++) {
-			td = document.createElement("td");
-
-			switch (j) {
-				case 0:
-					td.innerHTML = i + 1;
-					break;
-				default:
-					td.innerHTML = arr[i][j - 1];
-					break;
-			}
-
-			tr.appendChild(td);
-		}
-	}
-	table.appendChild(body);
-	$("#test").append(`<div>Таблица для построения диаграммы</div>`);
-	$("#test").append(table);
-}
-
-function calcThirdTable(arr) {
-	const array = [];
-	const n = values.length;
-
-	for (let i = 0; i < arr.length; i++) {
-		let temp = [];
-
-		temp.push((arr[i][1] - arr[i][0]).toFixed(decimals));
-		temp.push(
-			((Number(arr[i][1]) + Number(arr[i][0])) / 2).toFixed(decimals)
-		);
-		temp.push((Number(arr[i][2]) / n).toFixed(decimals));
-		temp.push((temp[2] / temp[0]).toFixed(decimals));
-
-		array.push(temp);
-	}
-
-	return array;
-}
-
-function unionIntervals(arr) {
-	let flag = 0;
-
-	for (let i = 0; i < arr.length; i++) {
-		if (i < arr.length - 1) {
-			if (arr[i][2] < crit) {
-				arr[i + 1][0] = arr[i][0];
-				arr[i + 1][2] = Number(arr[i][2]) + Number(arr[i + 1][2]);
-				arr.splice(i, 1);
-				flag = 1;
-				break;
-			}
-		} else {
-			if (arr[i][2] < crit) {
-				arr[i - 1][1] = arr[i][1];
-				arr[i - 1][2] = Number(arr[i][2]) + Number(arr[i - 1][2]);
-				arr.splice(i, 1);
-				flag = 1;
-				break;
-			}
-		}
-	}
-
-	if (flag) unionIntervals(arr);
-	else firstTable = arr;
-}
-
-function last() {
-	let ary = 0;
-	let n = values.length;
-	for (let i = 0; i < firstTable.length; i++) {
-		ary +=
-			Math.pow(firstTable[i][2] - n * lastArray[i][2], 2) /
-			(n * lastArray[i][2]);
-	}
-
-	$("#test").append(`<div>x2 = ${ary.toFixed(decimals)}</div>`);
-	let crit = prompt(
-		`введите х2крит q = ${q}, v = r - 1 - 2 = ${firstTable.length - 1 - 2}`
-	);
-	$("#test").append(`<div>критическое значение x2 = ${crit}</div>`);
-	$("#test").append(
-		`<div>${
-			ary < crit
-				? "x2 < x2крит => нормальное распределение"
-				: "x2 => x2крит => нормальное распределение не подтвердлается"
-		}</div>`
-	);
-}
-
 function calc() {
-	$("#test").append("Решение:");
-	let container = document.createElement("div");
-
-	const min = Math.min(...values);
-	const max = Math.max(...values);
-	const r = Math.ceil(Math.sqrt(values.length));
-	const h = (max - min) / r;
-	ra = r;
-	container.innerHTML =
-		repeat +
-		") " +
-		"Проверка гипотезы принадлежности результатов измерений к нормальному распределению" +
-		"<br>";
-
-	if (values.length > 50) {
-		container.innerHTML += `n = ${
-			values.length
-		} > 50 =>  ${katex.renderToString("\\chi^2")} Пирсона <br>`;
-		container.innerHTML += `${++repeat}) Диапазон результатов<br>${katex.renderToString(
-			`x_{min} = ${min}~${dimension}`
-		)}<br>${katex.renderToString(`x_{max} = ${max}~${dimension}`)}<br>`;
-
-		container.innerHTML += `${++repeat}) Число интервалов<br>`;
-		container.innerHTML += `r = ${katex.renderToString(
-			`\\sqrt{n} = \\sqrt{${values.length}} = ${r}`
-		)} <br>`;
-
-		container.innerHTML += `${++repeat}) Ширина интервала<br>`;
-		container.innerHTML += `${katex.renderToString(
-			`h = \\frac{x_{max} - x_{min}}{r} = \\frac{${max} - ${min}}{${r}} = ${h.toFixed(
-				decimals
-			)}~${dimension}`
-		)} <br>`;
-
-		container.innerHTML += `${++repeat}) Частота попадания в j-й интервал, где m<sub>j</sub> - число попавших значений.<br>`;
-		container.append(granicaTable(r, min, max, h));
-		$("#test").append(container);
-		unionTable(tempAr);
-		lastTable();
-		last();
-	} else
-		$("#test").innerHTML += `n = ${
-			values.length
-		} < 50 =>  ${katex.renderToString(
-			"\\chi^2"
-		)} Пирсона не может быть использован<br>`;
+	g();
+	if (system && borderSystem) showCorrection();
+	showResult();
 }
 
 function showValues() {
@@ -431,16 +452,26 @@ function getInputValues() {
 	dimension = $("#dimension").val();
 	values = values.map(value => Number(value));
 	q = Number($("#q").val());
+	system = Number(
+		$("#system")
+			.val()
+			.replace(/,/g, ".")
+			.match(/[+-]?([0-9]*[.])?[0-9]+/g)
+	);
+	borderSystem = Number(
+		$("#borderSystem")
+			.val()
+			.replace(/,/g, ".")
+			.match(/[+-]?([0-9]*[.])?[0-9]+/g)
+	);
 	repeat = 1;
 }
 
 $("#send").bind("click", function() {
 	getInputValues();
-
-	if (decimals && q && values && dimension) {
-		$("#test").empty();
-		$("#defaultValues").empty();
-		showValues();
-		calc();
-	}
+	showValues();
+	$("#test").empty();
+	$("#defaultValues").empty();
+	$("#test").append("Решение:");
+	calc();
 });
